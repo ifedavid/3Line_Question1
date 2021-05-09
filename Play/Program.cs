31b9e86f0ae6cde5c35d484b33238967534fc949
Ifeoluwa Onigbinde <onigbinde.i@gmail.com>
2021-05-09 16:36:19 +0100

    Add project files.

---
+++ Play/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Play
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! baby.");
+
+            Console.Read();
+        }
+    }
+
+    public class Question1
+    {
+
+        public int DifferentNumber(int[] array)
+        {
+
+            if (array.Length == 0) return 1;
+
+            HashSet<int> mySet = new HashSet<int>();
+
+
+            //Load all data in the array into a hashSet
+            foreach (int item in array)
+            {
+                mySet.Add(item);
+            }
+
+
+            //Find the minimum +ive element in O(n) time.
+            for (int i = 1; i < array.Length + 1; i++)
+            {
+                if (!mySet.Contains(i))
+                {
+                    return i;
+                }
+            }
+
+            return 1;
+
+        }
+    }
+}
---
+++ PlayTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Play;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Play.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void DifferentNumberTest1()
+        {
+            Question1 myProgram = new Question1();
+            int solution = myProgram.DifferentNumber(new int[] { -1, 2, 3, 4 });
+            Assert.AreEqual(1, solution);
+        }
+    }
+}
