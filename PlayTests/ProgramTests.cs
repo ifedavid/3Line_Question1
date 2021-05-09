0af762ed3fd78ad34e3d1f77921caac2ff22161b
Ifeoluwa Onigbinde <onigbinde.i@gmail.com>
2021-05-09 17:58:24 +0100

    Add more tests.

--- PlayTests/ProgramTests.cs
+++ PlayTests/ProgramTests.cs
@@ -10,10 +10,35 @@ namespace Play.Tests
     public class ProgramTests
     {
         [TestMethod()]
-        public void DifferentNumberTest1()
+        public void ContigousArray_Test()
         {
             Question1 myProgram = new Question1();
             int solution = myProgram.DifferentNumber(new int[] { -1, 2, 3, 4 });
+            Assert.AreEqual(1, solution);
+        }
+
+        [TestMethod()]
+        public void EmptyArray_Test()
+        {
+            Question1 myProgram = new Question1();
+            int solution = myProgram.DifferentNumber(new int[] { });
+            Assert.AreEqual(1, solution);
+        }
+
+        [TestMethod()]
+        public void ArbituaryElements_Test()
+        {
+            Question1 myProgram = new Question1();
+            int solution = myProgram.DifferentNumber(new int[] { 1, 24, 4, 3, 2, 65, 8 });
+            Assert.AreEqual(5, solution);
+        }
+
+
+        [TestMethod()]
+        public void NegativeElements_Test()
+        {
+            Question1 myProgram = new Question1();
+            int solution = myProgram.DifferentNumber(new int[] { -1, -4, -23, -34 });
             Assert.AreEqual(1, solution);
         }
     }
